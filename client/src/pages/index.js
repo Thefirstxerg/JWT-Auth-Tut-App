4c1b4b0308856c93eb92bebf3b444d6875100c1b
Thefirstxerg <thefirstxerg@gmail.com>
2025-07-08 13:29:43 +0200

    Update

---
+++ client/src/pages/index.js
@@ -0,0 +1,4 @@
+// src/pages/index.js
+export { default as Home }   from './Home';
+export { default as Login }  from './Login';
+export { default as Signup } from './Signup';
--- server/Routes/AuthRoute.js
+++ server/Routes/AuthRoute.js
@@ -1,6 +1,8 @@
 const { SignUp } = require('../Controllers/AuthController');
+const { userVerification } = require('../Middlewares/AuthMiddleware');
 const router = require('express').Router();
 
 router.post('/signup', SignUp);
+router.post('/', userVerification)
 
 module.exports = router;
